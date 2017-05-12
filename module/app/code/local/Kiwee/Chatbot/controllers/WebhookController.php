58ea04e4a489a7c14f4baae7c6af6cbc470b5266
Enzo Perrotta <ep@kendesign.it>
2017-05-12 15:13:43 +0200

    Testing buttons

--- module/app/code/local/Kiwee/Chatbot/controllers/WebhookController.php
+++ module/app/code/local/Kiwee/Chatbot/controllers/WebhookController.php
@@ -5,85 +5,40 @@ class Kiwee_Chatbot_WebhookController extends Mage_Core_Controller_Front_Action 
 	public function indexAction() {
 
 		$inputJson = file_get_contents('php://input');
-
 		$request = Mage::helper('core')->jsonDecode($inputJson);
-
-		error_log(var_export($request, true));
-
 		$intent = $request['result']['metadata']['intentName'];
 
-
+		error_log(var_export($request, true));
 		error_log($intent);
 
-		/*
+		$intent = 'products-intent';
+		switch($intent) {
+			case 'products-intent':
 
-{
-   "speech":"Here the products you requested:",
-   "displayText":"Here the products you requested:",
-   "data":{
-      "facebook":{
-         "attachment":{
-            "type":"template",
-            "payload":{
-               "template_type":"generic",
-               "elements":[
-                  {
-                     "title":"AVIATOR SUNGLASSES",
-                     "image_url":"https:\/\/vanilla.dev.kiwee.eu\/media\/catalog\/product\/cache\/2\/image\/9df78eab33525d08d6e5fb8d27136e95\/a\/c\/ace000a_1.jpg",
-                     "subtitle":"295.00 € - In stock",
-                     "buttons":[
-                     	{
-                     		"type":"web_url",
-                			"url":"https:\/\/vanilla.dev.kiwee.eu\/accessories\/eyewear\/aviator-sunglasses.html",
-                			"title":"Go to Website"
-                     	},
-                     	{
-							"type":"element_share"
-						}
-                     ]
-                  },
-                  {
-                     "title":"JACKIE O ROUND SUNGLASSES",
-                     "image_url":"https:\/\/vanilla.dev.kiwee.eu\/media\/catalog\/product\/cache\/1\/image\/9df78eab33525d08d6e5fb8d27136e95\/a\/c\/ace001_1.jpg",
-                     "subtitle":"225.00 € - In stock",
-                     "buttons":[
-                     	{
-                     		"type":"web_url",
-                			"url":"https:\/\/vanilla.dev.kiwee.eu\/accessories\/eyewear\/jackie-o-round-sunglasses.html",
-                			"title":"Go to Website"
-                     	},
-                     	{
-							"type":"element_share"
-						}
-                     ]
-                  }
-               ]
-            }
-         }
-      }
-   }
-}
-
-*/
-
-
-//		$response = Mage::helper('core')->jsonEncode($response);
-
-
-		$response = array(
-			"speech" => "Here the products you requested:",
-			"displayText" => "Here the products you requested:",
-			"data" => array(
-				"facebook" => array(
-					"attachment" => array(
-						"type" => "template",
-						"payload" => array(
-							"template_type" => "generic",
-							"elements" => array(
-								array(
-									"title" => "AVIATOR SUNGLASSES",
-									"image_url" => "https://vanilla.dev.kiwee.eu/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/a/c/ace000a_1.jpg",
-									"subtitle" => "In stock",
+				$response = array(
+					"speech" => "Here the products you requested:",
+					"displayText" => "Here the products you requested:",
+					"data" => array(
+						"facebook" => array(
+							"attachment" => array(
+								"type" => "template",
+								"payload" => array(
+									"template_type" => "generic",
+									"elements" => array(
+										array(
+											"title" => "AVIATOR SUNGLASSES",
+											"image_url" => "https://vanilla.dev.kiwee.eu/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/a/c/ace000a_1.jpg",
+											"subtitle" => "In stock",
+											"buttons"=> array(
+												array(
+													"type" => "web_url",
+													"url" => "https:\/\/vanilla.dev.kiwee.eu\/accessories\/eyewear\/jackie-o-round-sunglasses.html",
+													"title" => "Go to Website"
+												),
+												array(
+													"type" => "element_share"
+												)
+											)
 //									"default_action" => array(
 //										"type" => "web_url",
 //										"url" => "https://vanilla.dev.kiwee.eu/accessories/eyewear/aviator-sunglasses.html",
@@ -91,11 +46,11 @@ class Kiwee_Chatbot_WebhookController extends Mage_Core_Controller_Front_Action 
 //										"webview_height_ratio" => "tall",
 //										"fallback_url" => "https://vanilla.dev.kiwee.eu/accessories/eyewear/"
 //									)
-								),
-								array(
-									"title" => "AVIATOR SUNGLASSES",
-									"image_url" => "https://vanilla.dev.kiwee.eu/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/a/c/ace000a_1.jpg",
-									"subtitle" => "In stock",
+										),
+										array(
+											"title" => "AVIATOR SUNGLASSES",
+											"image_url" => "https://vanilla.dev.kiwee.eu/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/a/c/ace000a_1.jpg",
+											"subtitle" => "In stock",
 //									"default_action" => array(
 //										"type" => "web_url",
 //										"url" => "https://vanilla.dev.kiwee.eu/accessories/eyewear/aviator-sunglasses.html",
@@ -103,13 +58,21 @@ class Kiwee_Chatbot_WebhookController extends Mage_Core_Controller_Front_Action 
 //										"webview_height_ratio" => "tall",
 //										"fallback_url" => "https://vanilla.dev.kiwee.eu/accessories/eyewear/"
 //									)
+										)
+									)
 								)
 							)
 						)
 					)
-				)
-			)
-		);
+				);
+
+				break;
+		}
+
+//		$response = Mage::helper('core')->jsonEncode($response);
+
+
+
 
 		$response = Mage::helper('core')->jsonEncode($response);
 
